04e6f42ce0032518fea8a41b24b309f33a4c8497
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2015-05-12 15:21:19 -0300

    Removed legacy routes

--- app/controllers/routes.rb
+++ app/controllers/routes.rb
@@ -55,35 +55,4 @@ class Routes < Sinatra::Base
   get '/health' do
     "I'm OK"
   end
-
-  # Show all sources and its respective values
-  # to a date.
-  # http://localhost:8080/report/dashboard/date/2015-04-25
-  get '/report/dashboard/date/:date' do
-    begin
-      { status: true, data: OrdersByDay.dashboard(params['date']) }.to_json
-    rescue => e
-      { status: false, message: e.message }.to_json
-    end
-  end
-
-  # Show all orders and upsells from a source to a date.
-  # localhost:8080/report/source/Social%20Media/date/2015-03-19
-  get '/report/source/:name/date/:date' do
-    begin
-      { status: true, orders: OrdersByDay.from_source(params['name'], params['date']) }.to_json
-    rescue => e
-      { status: false, message: e.message }.to_json
-    end
-  end
-
-  # Show all orders and upsells from a source to a date.
-  # localhost:8080/report/source/Social%20Media/date/2015-03-19
-  get '/report/tracker/:name/date/:date' do
-    begin
-      { status: true, data: OrdersByDay.from_tracker(params['name'], params['date']) }.to_json
-    rescue => e
-      { status: false, message: e.message }.to_json
-    end
-  end
 end
