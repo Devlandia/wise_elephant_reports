78f6504ab553f4c4188ca24d3c06032e1006887e
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2015-05-13 15:00:40 -0300

    Client requests

--- app/helpers/results_parser_helper.rb
+++ app/helpers/results_parser_helper.rb
@@ -1,6 +1,6 @@
 module ResultsParser
   def count_totals(items)
-    response  = { hits: 0, conversions: 0, upsells: 0, sales: 0, total_upsells: 0, total_sales: 0, avg_order_value: 0, hits_to_orders: 0 }
+    response  = { hits: 0, conversions: 0, upsells: 0, sales: 0, total_upsells: 0, total_sales: 0, avg_order_value: 0, hits_to_orders: 0, value_per_hit: 0 }
 
     items.each { |source, values| values.each { |key, value| response[key] += value } }
 
@@ -9,6 +9,7 @@ module ResultsParser
 
     response[:avg_order_value]  = total_orders == 0     ? 0 : value_orders / total_orders
     response[:hits_to_orders]   = response[:hits] == 0  ? 0 : total_orders / response[:hits]
+    response[:value_per_hit]    = response[:hits] == 0  ? 0 : response[:total_sales] / response[:hits]
 
     response
   end
@@ -26,7 +27,10 @@ module ResultsParser
         total_sales:      values[:total_sales] + values[:total_upsells],
         hits_to_orders:   values[:conversions],
         avg_order_value:  values[:avg_order_value],
+        value_per_hit:    0,
       }
+
+      response[source][:value_per_hit]  = values[:total_sales] / values[:hits] unless values[:hits] == 0;
     end
 
     response
